56a9f31915c4a3b349ec851c57cee05d3d636042
Solo-Hawk <Solo-Hawk@users.noreply.github.com>
2019-01-24 16:08:35 +0000

    Setting up
    Starting this top down shooter

---
+++ js/main.js
@@ -0,0 +1,98 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {
+                y: 0
+            } // Top down game, so no gravity
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+
+var game = new Phaser.Game(config)
+
+function preload(){
+
+  this.load.image('player-body', 'assets/tank/player/body.png');
+  this.load.image('player-barrel-1', 'assets/tank/player/barrel1.png');
+
+
+  this.load.image('enemy-body', 'assets/tank/enemy/body.png')
+  this.load.image('enemy-barrel-1', 'assets/tank/enemy/barrel1.png')
+  this.load.image('enemy-barrel-2', 'assets/tank/enemy/barrel2.png')
+  this.load.image('enemy-barrel-3', 'assets/tank/enemy/barrel3.png')
+
+  this.load.image('enemy-heavy-body', 'assets/tank/enemy/heavybody.png')
+  this.load.image('enemy-heavy-barrel-1', 'assets/tank/enemy/heavybarrel1.png')
+  this.load.image('enemy-heavy-barrel-2', 'assets/tank/enemy/heavybarrel2.png')
+
+  this.load.image('bullet1', 'assets/projectiles/shotOrange.png')
+  this.load.image('bullet2', 'assets/projectiles/shotRed.png')
+
+  this.load.image('tracks', 'assets/effects/tracksSmall.png')
+
+  this.load.atlas({
+    key: 'explosions',
+    textureURL: 'assets/effects/explosion-spritesheet.png',
+    atlasURL: 'assets/effects/explosion-spritesheet.json'
+  });
+
+  this.load.atlas({
+    key: 'smokes',
+    textureURL: 'assets/effects/smoke-spritesheet.png',
+    atlasURL: 'assets/effects/smoke-spritesheet.json'
+  });
+
+  this.load.image('terrain', 'assets/map/terrain.png');
+  this.load.tilemapTiledJSON({
+    key: 'map',
+    url: 'assets/map/map.json'
+  })
+
+
+}
+
+function create(){
+
+  //-------------//
+  console.log(this)
+}
+
+function update(){
+
+}
+
+//----------------------------------------------//
+//----------------------------------------------//
+
+function createAnims(){
+  this.anims.create({
+    key: 'boom',
+    frames: this.anims.generateFrameNames('explosions', {
+      start: 1, end: 5,
+      prefix: 'explosion', suffix: ''
+    }),
+    frameRate: 16,
+    repeat: 0
+  });
+
+  this.anims.create({
+    key: 'smoke',
+    frames: this.anims.generateFrameNames('smokes', {
+      start: 1, end: 5,
+      prefix: 'smoke', suffix: ''
+    }),
+    frameRate: 16,
+    repeat: 0
+  });
+}
